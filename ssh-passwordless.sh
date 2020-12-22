0f14cc12f2ec0ff3688bfc40294bef11129d46bb
Amir Sasani <sasani.amir.1998@gmail.com>
2020-12-22 18:29:08 -0500

    Update ssh-passwordless.sh

--- ssh-passwordless.sh
+++ ssh-passwordless.sh
@@ -5,7 +5,7 @@ read -p "Your username on server: " SERVER_USER
 read -p "Your server's IP address: " SERVER_IP
 read -sp "${SERVER_USER}'s password on server: " SERVER_PASSWORD
 
-SERVER_ADDRESS="$SERVER_USER@${SERVER_IP}"
+SERVER_ADDRESS="${SERVER_USER}@${SERVER_IP}"
 
 ssh-keygen -t rsa;
 ssh $SERVER_ADDRESS mkdir -p .ssh;
